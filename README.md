c24af75c07d49940df39fb7db8b6e5825d2f822f
yugmani <yugmani@yahoo.com>
2021-06-06 00:50:17 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-props-default
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-props-default)
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Default Props</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <p>
+        <a href="https://www.geeksforgeeks.org/reactjs-props-set-2/?ref=rp">
+          ReactJS | Props – Set 2 (Default Props)
+        </a>
+      </p>
+    </div>
+  );
+}
